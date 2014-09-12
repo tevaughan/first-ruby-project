ed8e6687819d76ed0b9b0044995639d799f3e646
Thomas E. Vaughan <tevaughan@gmail.com>
2014-09-12 08:20:43 -0600

    Added comments. Now every option has a default value.

--- gen-parabola-cmdline.rb
+++ gen-parabola-cmdline.rb
@@ -3,11 +3,12 @@ require 'optparse'
 require 'ostruct'
 
 # Representation of the command-line for the parabola-generator program.
+#
+# The key word 'self' in the method declarations indicates that the method
+# belongs to the class (and not to an instance). This is like a static method
+# in C++.
 class GenParabolaCmdline
    # Construct a parser for the command line, and then run the parser.
-   #
-   # The key word 'self' here indicates that 'parse' is a method of the class
-   # (and not of an instance).
    #
    # +args+:: List of command-line arguments to the program. ARGV is passed in
    #          here.
@@ -16,19 +17,6 @@ class GenParabolaCmdline
       parser = self.makeParser(options)
       parser.summary_width = 20
       parser.parse!(args)  # args (ARGV) will have options removed!
-      if options.n < 1
-         puts "GenParabolaCmdline::parse: ERROR: " +
-              "Number of steps must be specified.\n\n"
-         puts parser
-         exit 1
-      end
-      if options.b == options.e
-         puts "GenParabolaCmdline::parse: ERROR: " +
-              "(beg, end) must be different, not " +
-              "(#{options.b}, #{options.e})\n\n"
-         puts parser
-         exit 1
-      end
       options
    end
 
@@ -42,73 +30,92 @@ class GenParabolaCmdline
       options.x = 0.0  # Default x position of vertex is 0.0.
       options.y = 0.0  # Default y position of vertex is 0.0.
       options.b = 0.0  # Default beginning x is 0.0.
-      options.e = 0.0  # Default ending    x is 0.0.
-      options.n = 0    # Default number of steps is 0, an illegal value.
-      return options
+      options.e = 1.0  # Default ending    x is 0.0.
+      options.n = 10   # Default number of steps is 10.
+      options
    end
 
-   def self.makeWidthParser(opts, options)
+   # Define what to do when the width is encountered.
+   # +parser+::  Parser to which definition will be attached.
+   # +options+:: Option values loaded with defaults on entry.
+   def self.widthHandler(parser, options)
       wdsc = "From vertex, change in x for unit change in y."
       wdef = "By default, W=#{options.w}"
-      opts.on("-w", "--width W", Float, wdsc, wdef) do |w|
+      parser.on("-w", "--width W", Float, wdsc, wdef) do |w|
          if w <= 0.0
             puts "GenParabolaCmdline::parse: ERROR: " +
                  "Width must be positive, not #{w}\n\n"
-            puts opts
+            puts parser
             exit 1
          end
          options.w = w
       end
    end
 
-   def self.makeXvertParser(opts, options)
+   # Define what to do when the horizontal coordinate of the vertex is
+   # encountered.
+   # +parser+::  Parser to which definition will be attached.
+   # +options+:: Option values loaded with defaults on entry.
+   def self.xVertHandler(parser, options)
       xdsc = "X coordinate of vertex."
       xdef = "By default, X=#{options.x}"
-      opts.on("-x", "--x-coord X", Float, xdsc, xdef) do |x|
+      parser.on("-x", "--x-coord X", Float, xdsc, xdef) do |x|
          options.x = x
       end
    end
 
-   def self.makeYvertParser(opts, options)
+   # Define what to do when the vertical coordinate of the vertex is
+   # encountered.
+   # +parser+::  Parser to which definition will be attached.
+   # +options+:: Option values loaded with defaults on entry.
+   def self.yVertHandler(parser, options)
       ydsc = "Y coordinate of vertex."
       ydef = "By default, Y=#{options.y}"
-      opts.on("-y", "--y-coord Y", Float, ydsc, ydef) do |y|
+      parser.on("-y", "--y-coord Y", Float, ydsc, ydef) do |y|
          options.y = y
       end
    end
 
-   def self.makeXbegParser(opts, options)
+   # +parser+::  Parser to which definition will be attached.
+   # +options+:: Option values loaded with defaults on entry.
+   def self.xBegHandler(parser, options)
       bdsc = "Beginning x coordinate."
       bdef = "By default, B=#{options.b}"
-      opts.on("-b", "--x-beg B", Float, bdsc, bdef) do |b|
+      parser.on("-b", "--x-beg B", Float, bdsc, bdef) do |b|
          options.b = b
       end
    end
 
-   def self.makeXendParser(opts, options)
+   # +parser+::  Parser to which definition will be attached.
+   # +options+:: Option values loaded with defaults on entry.
+   def self.xEndHandler(parser, options)
       edsc = "Ending x coordinate."
       edef = "By default, E=#{options.e}"
-      opts.on("-e", "--x-end E", Float, edsc, edef) do |e|
+      parser.on("-e", "--x-end E", Float, edsc, edef) do |e|
          options.e = e
       end
    end
 
-   def self.makeNumstepsParser(opts, options)
+   # +parser+::  Parser to which definition will be attached.
+   # +options+:: Option values loaded with defaults on entry.
+   def self.numStepsHandler(parser, options)
       ndsc = "Number of steps between B and E."
-      opts.on("-n", "--num-steps N", Integer, ndsc) do |n|
+      ndef = "By default, N=#{options.n}"
+      parser.on("-n", "--num-steps N", Integer, ndsc, ndef) do |n|
          if n < 1
             puts "GenParabolaCmdline::parse: ERROR: " +
                  "number of steps must be positive, not #{n}\n\n"
-            puts opts
+            puts parser
             exit 1
          end
          options.n = n
       end
    end
 
-   def self.makeHelpParser(opts, options)
-      opts.on_tail("-h", "--help", "Show this message.") do
-         puts opts
+   # +parser+::  Parser to which definition will be attached.
+   def self.helpHandler(parser)
+      parser.on("-h", "--help", "Show this message.") do
+         puts parser
          exit 0
       end
    end
@@ -118,9 +125,8 @@ class GenParabolaCmdline
    # +options+:: Options to be set by parser. Default values have already been
    #             loaded in options before makeParser is called.
    def self.makeParser(options)
-      return OptionParser.new do |opts|
-         opts.banner = "Usage:  #{$0} [-w W] [-x X] [-y Y] [-b B] [-e E] -n N"
-         opts.separator <<EOF
+      return OptionParser.new do |parser|
+         parser.separator <<EOF
 
 Send to standard output the coordinates of points along a parabola.  Each line
 of output contains one pair of coordinates separated by white space.
@@ -128,13 +134,13 @@ of output contains one pair of coordinates separated by white space.
 Options:
 
 EOF
-         self.makeWidthParser(opts, options)
-         self.makeXvertParser(opts, options)
-         self.makeYvertParser(opts, options)
-         self.makeXbegParser(opts, options)
-         self.makeXendParser(opts, options)
-         self.makeNumstepsParser(opts, options)
-         self.makeHelpParser(opts, options)
+         self.widthHandler(parser, options)
+         self.xVertHandler(parser, options)
+         self.yVertHandler(parser, options)
+         self.xBegHandler(parser, options)
+         self.xEndHandler(parser, options)
+         self.numStepsHandler(parser, options)
+         self.helpHandler(parser)
       end
    end
 end
